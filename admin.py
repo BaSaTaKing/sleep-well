62f7f2b8cc9149b4c2cbd21ea377dc793aa617d3
BaSaTaKing <154591053+BaSaTaKing@users.noreply.github.com>
2026-04-29 17:53:00 +0800

    Add files via upload
    用于追踪睡眠时间表的 React + Django 应用
    
    前端和用户界面设计
    
    启动画面：****当用户未登录时，显示主页，介绍该应用程序及其如何帮助用户更好地跟踪睡眠。
    注册页面：可以使用 Google 或 Facebook 的身份验证库，或者实现您自己的身份验证模块。
    登录页面：用户应该能够使用其注册方式登录帐户。
    新用户登录：显示一个空白页面，其中包含“新建条目”按钮。用户点击“新建条目”后，显示一个模态对话框/弹出窗口：
    使用日期选择器选择日期
    选择睡眠时间（小时和分钟）
    选择起床时间（小时和分钟）。
    计算总睡眠时间
    添加重置、取消和提交按钮
    老用户登录：显示最近 7 天或 1 个月的登录记录列表
    清单应包括：日期、入睡时间、起床时间和睡眠时长
    使用图形库创建一个浮动条形图，其中 Y 轴表示睡眠时长，X 轴表示日期。
    显示“新建条目”按钮
    后端
    
    所有条目必须存储在服务器的持久存储中，以便随时取用。使用数据库和 REST API 获取数据。
    编写后端 API 以读取、写入和更新条目。

---
+++ admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import  admin
+from .models import *
+
+admin.site.register(User)
+admin.site.register(HealthInfo)
+# 超级管理员用户密码
+# admin  qwe123456
